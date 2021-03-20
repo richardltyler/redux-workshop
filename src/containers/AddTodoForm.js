1f845d8907129222e975d642c4b7291b28ee7ea5
Richard Tyler <richardtyler2011@gmail.com>
2021-03-20 10:41:37 -0600

    Wrap addTodoForm in connect

--- src/containers/AddTodoForm.js
+++ src/containers/AddTodoForm.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { addToDo } from '../actions';
+import { connect } from 'react-redux';
 
 class AddTodoForm extends Component {
   constructor(props) {
@@ -11,7 +13,8 @@ class AddTodoForm extends Component {
   }
 
   submitForm = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    this.props.addToDo(this.state.todo);
     this.setState({ todo: '' });
   }
 
@@ -32,4 +35,8 @@ class AddTodoForm extends Component {
   }
 }
 
-export default AddTodoForm;
+const mapDispatchToProps = dispatch => ({
+    addToDo: text => dispatch( addToDo(text) )
+})
+
+export default connect(null, mapDispatchToProps)(AddTodoForm);
